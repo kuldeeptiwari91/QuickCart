a3df65f0c4f5ee272b1e3c9d64c4221f6f98f040
Kuldeep Tiwari <kuldeeptiwari9162@gmail.com>
2025-03-08 20:31:55 +0530

    Added the Header and banner

--- app/src/main/java/com/kuldeep/quickcart/pages/HomePage.kt
+++ app/src/main/java/com/kuldeep/quickcart/pages/HomePage.kt
@@ -1,10 +1,26 @@
 package com.kuldeep.quickcart.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kuldeep.quickcart.components.BannerView
+import com.kuldeep.quickcart.components.HeaderView
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Text(text = "Home Page")
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        HeaderView(modifier)
+        Spacer(modifier = Modifier.height(10.dp))
+        BannerView(modifier = Modifier.height(300.dp))
+    }
 }
