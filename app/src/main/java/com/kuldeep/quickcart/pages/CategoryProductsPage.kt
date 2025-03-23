1b70554e8973c7f1a7d85479a1bdd9f360f2f757
Kuldeep Tiwari <kuldeeptiwari9162@gmail.com>
2025-03-23 23:20:32 +0530

    Added the Product Category
    Added the product category feature and implemented how to fetch the products based on its category.

--- app/src/main/java/com/kuldeep/quickcart/pages/CategoryProductsPage.kt
+++ app/src/main/java/com/kuldeep/quickcart/pages/CategoryProductsPage.kt
@@ -1,10 +1,50 @@
 package com.kuldeep.quickcart.pages
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.kuldeep.quickcart.model.CategoryModel
+import com.kuldeep.quickcart.model.ProductModel
 
 @Composable
-fun CategoryProductsPage(modifier: Modifier = Modifier,categoryId:String) {
-    Text(text = "Product Category page ::-> $categoryId")
+fun CategoryProductsPage(modifier: Modifier = Modifier, categoryId: String) {
+    val productList = remember {
+        mutableStateOf<List<ProductModel>>(emptyList())
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        Firebase.firestore.collection("data").document("stock")
+            .collection("products")
+            .whereEqualTo("category", categoryId)
+            .get().addOnCompleteListener() {
+                if (it.isSuccessful) {
+                    val resultList = it.result.documents.mapNotNull { doc ->
+                        doc.toObject(ProductModel::class.java)
+                    }
+                    productList.value = resultList
+                }
+            }
+    }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        items(productList.value) { item ->
+            Text(text = item.title)
+            Spacer(modifier = Modifier.height(50.dp))
+        }
+    }
 }
