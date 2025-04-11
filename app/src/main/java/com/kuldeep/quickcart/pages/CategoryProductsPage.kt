723feb8325df84705d1fba955c1d3029b36eda9f
Kuldeep Tiwari <kuldeeptiwari9162@gmail.com>
2025-04-11 17:58:26 +0530

    Product View Design
    Implemented the Product View Design and what will be shown in the product Items

--- app/src/main/java/com/kuldeep/quickcart/pages/CategoryProductsPage.kt
+++ app/src/main/java/com/kuldeep/quickcart/pages/CategoryProductsPage.kt
@@ -1,12 +1,11 @@
 package com.kuldeep.quickcart.pages
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -15,8 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
-import com.kuldeep.quickcart.model.CategoryModel
+import com.kuldeep.quickcart.components.ProductItemView
 import com.kuldeep.quickcart.model.ProductModel
+import okhttp3.internal.wait
 
 @Composable
 fun CategoryProductsPage(modifier: Modifier = Modifier, categoryId: String) {
@@ -42,9 +42,15 @@ fun CategoryProductsPage(modifier: Modifier = Modifier, categoryId: String) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        items(productList.value) { item ->
-            Text(text = item.title)
-            Spacer(modifier = Modifier.height(50.dp))
+        items(productList.value.chunked(2)) { rowItem ->
+            Row {
+                rowItem.forEach {
+                    ProductItemView(product = it, modifier = Modifier.weight(1f))
+                }
+                if (rowItem.size == 1) {
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
         }
     }
 }
